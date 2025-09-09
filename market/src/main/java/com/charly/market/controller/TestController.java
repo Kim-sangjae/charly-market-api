7eb24bea3dbfe832bc0691804de5ca8bebe78955
Kim-sangjae <kimsangjae94114@gmail.com>
2025-09-09 20:56:37 +0900

    charlyMarket first commit

--- market/src/main/java/com/charly/market/controller/TestController.java
+++ market/src/main/java/com/charly/market/controller/TestController.java
@@ -1,19 +1,28 @@
 package com.charly.market.controller;
 
+import com.charly.market.dto.CategoryDto;
+import com.charly.market.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/test")
 public class TestController {
 
+    private final TestService testService;
+
     @GetMapping("/get-test")
     public ResponseEntity<String> getTest(){
         return ResponseEntity.ok("Test");
+    }
+
+    @PostMapping("/category")
+    public ResponseEntity<String> createCategory(@RequestBody CategoryDto categoryDto){
+        testService.createCategory(categoryDto);
+        
+        return ResponseEntity.ok("Category");
     }
 
 }
